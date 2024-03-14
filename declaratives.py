b00cdd7cfd490b8bdefa043ffb8ccf07de897f62
Leo <leonardo_as20@hotmail.com>
2024-03-14 20:10:54 -0300

    Criando banco

--- declaratives.py
+++ declaratives.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy as sqla
@@ -8,7 +8,17 @@ import os
 
 local_path = os.getcwd()
 
-archive = 'test.db'
-engine = create_engine(f'sqlite:///{os.path.join(local_path,archive)}', echo=True)
+archive = 'demo.db'
+engine = create_engine(f'sqlite:///{os.path.join(local_path,archive)}')
+engine.begin()
 
+Session = sessionmaker(bind=engine)
 Base = declarative_base()
+metadata = MetaData()
+
+
+def remove_table(table, metadata = metadata, engine = engine):
+    metadata.reflect(engine)
+    tables_disponiveis = metadata.tables
+    if table in tables_disponiveis:
+        tables_disponiveis[table].drop(engine)
--- tables.py
+++ tables.py
@@ -1,18 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import sqlalchemy as sqla
-import pandas as pd
+from declaratives import *
 
-
-engine = create_engine('sqlite:///test.db', echo=True)
-
-Base = declarative_base()
-
-class Filme(Base):
-    __tablename__ = 'filme'
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    titulo = Column(String, nullable=False)
-    ano_lancamento = Column(Integer)
-    pontuacao = Column(Integer)
+    name = Column(String)
+    fullname = Column(String)
 
